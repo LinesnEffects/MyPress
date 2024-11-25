342180593e93f903bd3e25fd3fe1dbde3141dd5f
LinesnEffects <bgf2311@outlook.com>
2024-11-25 14:02:20 -0500

    Created form, implemented toast component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,15 +20,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+        <Navbar />
         <div className="flex">
-          <div className='hidden md:block h-[100vh] w-[300px]'>
-            <Sidebar/>
+          <div className="hidden md:block h-[100vh] w-[300px]">
+            <Sidebar />
           </div>
-          <div className="p-5 w-full md:max-w-[1140px]">
-            {children}
-          </div>
+          <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
         </div>
+        <Toaster />
       </body>
     </html>
   );
